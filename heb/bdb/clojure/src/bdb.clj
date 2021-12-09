fc44f0c28bfe725203858964719994f52baaaddf
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-12-08 21:48:06 -0500

    Better algorithm for extracting lemma

--- heb/bdb/clojure/src/bdb.clj
+++ heb/bdb/clojure/src/bdb.clj
@@ -1,9 +1,11 @@
 (ns bdb
   (:require
    [clojure.java.io :as io]
+   [clojure.core.reducers :as r]
+   [clojure.string :as strings]
    [clojure.data.json :as json]
    [tupelo.parse.xml :as txml]
-   [defun.core :refer-macros  [defun]])
+   [defun.core :refer [defun]])
   (:import java.io.StringReader))
 
 (def bdb-xml "/Users/jonathanwhittle/Development/lexica/heb/bdb/BrownDriverBriggs.xml")
@@ -17,13 +19,6 @@
 (defn nofile! []
   (println (str (error "ERROR") " Filepath is required (" (info "clj -X:run :file ../path.json") ")"))
   (System/exit 1))
-
-(defn print->currentdir []
-  (println (str "Current Working Directory: " (info (System/getProperty "user.dir")))))
-
-(defn print->file [f]
-  (println (str "File: " (info f) "\n")))
-
 (defn file->name [f] (.getName (io/file f)))
 (defn file->exists [f] (.exists (io/file f)))
 (defn file->abspath [f] (.getAbsolutePath (io/file f)))
@@ -46,48 +41,111 @@
 ;;     -entry
 ;;     -entry
 ;;     -entry
-(defrecord Entry [meta letter language type allRefs mod])
+(defrecord Entry [id letter language type dagger mod definition])
 
 (defn from-attrs [k unit] (k (:attrs unit)))
 (defn nil-default [val def]
-	(if (nil? val) def val))
+  (if (nil? val) def val))
 
-(defn lex->parse [lexicon]	
+(defun entry->lemma
+  "Extract lemma from the entry.
+	 	If the first entry is a string, skip to second and extract from :content.
+	 	Some entries begin with a '[', so this must be skipped to extract the lemma."
+  ([(f :guard #(string? %)), s] (first (:content s)))
+  ([f _] (first (:content f)))
+  ([_ _] ""))
+
+(defun language->full
+  (["heb"] "hebrew")
+  (["arc"] "aramaic")
+  ([_] "hebrew"))
+
+(defun entry->tostr
+  "Extracts data from an entry's content and formats it"
+  ([:w _ content] (strings/trim (first content)))
+  ([:pos _ content] (first content))
+  ([:def _ content] (first content))
+  ([:sense _ content] ;; can contain - :pos,:stem,:asp,:def,:em,:w,:ref
+   (if (string? content)
+     (do content)
+     (do
+       (strings/join " " (map
+                          (fn [child]
+                            (let [tag (:tag child)
+                                  attrs (:attrs child)
+                                  sense-content (:content child)]
+                              (entry->tostr tag attrs sense-content)))
+                          content)))))
+  ([:stem _ content] (str  "\n" (first content)))
+  ([:asp _ content] (first content))
+  ([:em _ content] (first content))
+  ([:ref _ content] (first content))
+  ([:foreign _ content] (first content))
+  ([_ _ _] ""))
+
+(defn entry->raw [contents]
+  "Extract the raw content of the entry"
+  (reduce
+   (fn [acc c]
+     (if (string? c)
+       (do (str acc (strings/trim c) " "))
+       (do
+         (let [tag (:tag c)
+               attrs (:attrs c)
+               content (:content c)]
+           (str acc (entry->tostr tag attrs content) " ")))))
+   ""
+   contents))
+
+(defn entry->expand [part-id section-id dict lang]
+  (fn [entry]
+    (let [entry-id (from-attrs :id entry)
+          type (from-attrs :type entry)
+          dagger (from-attrs :cite entry)
+          mod (from-attrs :mod entry)
+          content (:content entry)
+          lemma (entry->lemma (first content) (second content))]
+      {:id {:part part-id, :section section-id, :entry entry-id}
+       :dict dict
+       :lemma lemma
+       :language (language->full lang)
+       :root (if (= "root" type) true false)
+       :dagger (if (= "full" dagger) true false)
+       :mod (nil-default mod "I")
+       :entry (strings/trim (entry->raw content))})))
+
+(defn section->expand [part-id letter lang]
+  (fn [section]
+    (let [section-id (from-attrs :id section)
+          entries (:content section)]
+      (map (entry->expand part-id section-id letter lang) entries))))
+
+(defn part->expand [part]
+  (let [part-id (from-attrs :id part)
+        letter (from-attrs :title part)
+        lang (from-attrs :xml:lang part)
+        sections (:content part)]
+    (mapcat (section->expand part-id letter lang) sections)))
+
+(defn lex->parse [lexicon]
+  "Parse the Lexicon. mapcat until entry, collecting part and section downward"
   (let [parts (:content lexicon)]
-  	(mapcat 
-  		(fn [part] 
-  			(let [part-id (from-attrs :id part)
-          	letter (from-attrs :title part)
-         		lang (from-attrs :xml:lang part)
-         	 sections (:content part)]
-  				(mapcat 
-  					(fn [section] 
-  						(let [section-id (from-attrs :id section)
-  												entries (:content section)] 
-  												(map
-  													(fn [entry] 
-  														(let [entry-id (from-attrs :id entry)
-  																				type (from-attrs :type entry)
-  																				allRefs (from-attrs :cite entry)
-  																				mod (from-attrs :mod entry)]
-  															(->Entry 
-  																{:partID part-id, :sectionID section-id, :entryID entry-id} 
-  																letter 
-  																lang 
-  																type 
-  																(nil-default allRefs false)
-  																(nil-default mod "I"))))
-  													entries)))
-  					sections)))
-  		parts)))
+    (mapcat part->expand parts)))
 
-(defn xml [& a]
+(defn run [& a]
+  "Entrypoint for BDB xml"
   (let [args (remove nil? a)]
     (if (empty? args)
       (do (nofile!))
       (do
-        (let [bdb (txml/parse (slurp bdb-xml))]
-          (println (first (lex->parse bdb))))))))
+        (let [bdb (txml/parse (slurp bdb-xml))
+              parsed (lex->parse bdb)
+              topten (take 10 parsed)]
+          (json/pprint topten :escape-unicode false)
+          ; (json/pprint (nth parsed 101) :escape-unicode false)
+          ; (json/pprint (nth parsed 102) :escape-unicode false)
+          ; (json/pprint (nth parsed 103) :escape-unicode false)
+          )))))
 
 
 
